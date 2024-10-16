563ca617cbd44f3b446cca904ee5f050209be80d
Platon <platon20081019@gmail.com>
2024-10-16 23:45:17 +0300

    Please enteras

--- back/index.js
+++ back/index.js
@@ -1,3 +1,10 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
