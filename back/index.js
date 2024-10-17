3d8cc65bc0962d058819346229a7420328166861
Platon <platon20081019@gmail.com>
2024-10-17 22:29:48 +0300

    login and registration

--- back/index.js
+++ back/index.js
@@ -42,7 +42,7 @@ app.post("/create-account", async (req, res) => {
   }
   const isUser = await User.findOne({ email: email });
 
-  if (!isUser) {
+  if (isUser) {
     return res.json({
       error: true,
       message: "User is alredy exist",
@@ -73,6 +73,47 @@ app.post("/create-account", async (req, res) => {
     accessToken,
     message: "Register successful",
   });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({
+      message: "Email is required",
+    });
+  }
+
+  if (!password) {
+    return res.status(400).json({
+      message: "Password is required",
+    });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+
+    return res.json({
+      error: false,
+      message: "Login Successful",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid",
+    });
+  }
 });
 
 console.log("http://localhost:8000");
