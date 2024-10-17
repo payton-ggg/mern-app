4eb43e1c238e257dc3ff83926daed1440c340443
Platon <platon20081019@gmail.com>
2024-10-17 11:18:25 +0300

    asd

--- back/index.js
+++ back/index.js
@@ -8,6 +8,8 @@ mongoose.connect(config.connectionString);
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
@@ -19,6 +21,23 @@ app.use(
 
 app.get("/", (req, res) => {
   res.json({ data: "hello" });
+});
+
+app.post("/create-account", async (req, res) => {
+  const { fullname, email, password } = req.body;
+  if (!fullname) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full name is required" });
+  }
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
 });
 
 console.log("http://localhost:8000");
